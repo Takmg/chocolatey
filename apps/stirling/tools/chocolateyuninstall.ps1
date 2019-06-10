e64a1d1ef74f400010ffd01b130d77044e2202fb
takmg <takmg@users.noreply.github.com>
2019-06-10 19:12:02 +0900

    Shotcutの削除処理を追加

--- apps/stirling/tools/chocolateyuninstall.ps1
+++ apps/stirling/tools/chocolateyuninstall.ps1
@@ -1,14 +1,13 @@
 $ErrorActionPreference = 'Stop'
  
 $installPath = Join-Path (Get-ToolsLocation) $env:ChocolateyPackageName
- 
+
 if (Test-Path $installPath) {
+    # Remove Shortcut
+    $exename = Get-ChildItem( Join-Path $installPath "*.exe")
+    $lnkpath = Join-Path ([Environment]::GetFolderPath('Desktop')) ($exename[0].BaseName + ".lnk")
+    Remove-Item -path $lnkpath -Force
+
+    # Remove Install File
     Remove-Item -path $installPath -Recurse -Force
 }
-
-# no remove shortcut 
-Write-Host ''
-Write-Host '---------------------------------------------------'
-Write-Host "Please manually delete the stirling.lnk shortcut created on the desktop when installed."
-Write-Host '---------------------------------------------------'
-Write-Host ''
