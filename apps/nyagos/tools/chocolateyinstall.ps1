c5a3a02131a418488a6455ca1188003f8ca90377
might <takmg@norepry.github.com>
2019-12-12 10:39:15 +0900

    nyagosのパッケージアップデート

--- apps/nyagos/tools/chocolateyinstall.ps1
+++ apps/nyagos/tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $destination
     softwareName   = 'nyagos*'
-    url            = 'https://github.com/zetamatta/nyagos/releases/download/4.4.5_0/nyagos-4.4.5_0-windows-386.zip'
-    url64bit       = 'https://github.com/zetamatta/nyagos/releases/download/4.4.5_0/nyagos-4.4.5_0-windows-amd64.zip'
+    url            = 'https://github.com/zetamatta/nyagos/releases/download/4.4.5_2/nyagos-4.4.5_2-windows-386.zip'
+    url64bit       = 'https://github.com/zetamatta/nyagos/releases/download/4.4.5_2/nyagos-4.4.5_2-windows-amd64.zip'
     checksumType   = 'sha256' 
     checksumType64 = 'sha256' 
-    checksum       = 'E5924AECCA9E8A142B6185F7068C04A1D75D44A0B3B91039C5CFA75D898CA458'
-    checksum64     = 'CB68795A05C4632E906AF335FA413612B7897E4ABA9E6754635B204AA69F42ED'
+    checksum       = '412d66a95fd34bec825a7579335a59724125977c7ce55588bcaf81e918633d63'
+    checksum64     = 'edc0f1411344a1b0b2b675e1609264a7cf670ae6895cf6a33777d90688d42f1a'
 }
 
 # Install nyagos
