95eb0e2deaed5805cb38e78f53f2b78cd348fc53
takmg <takmg@norepry.github.com>
2022-09-22 18:52:04 +0900

    update update.ps1

--- apps/awssamcli/update.ps1
+++ apps/awssamcli/update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://github.com/aws/aws-sam-cli/releases'
+$releases = 'https://api.github.com/repos/aws/aws-sam-cli/releases/latest'
 
 function global:au_SearchReplace {
    @{
@@ -12,16 +12,13 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $download_data = Invoke-WebRequest -Uri $releases -UseBasicParsing | ConvertFrom-Json
+    $url64 = $download_data.assets | ? name -match "AWS_SAM_CLI_64_PY3" | select -First 1  -expand browser_download_url
+    $version = $download_data.tag_name
+    $version = $version -replace 'v' , ''
 
-    $url64 = $download_page.links | ? href -match 'AWS_SAM_CLI_64_PY3.msi' | select -First 1 -expand href
-    $version = (Split-Path ( Split-Path $url64 ) -Leaf)
-    $version = $version.Remove(0,1)
-    $version = $version -replace '_', '.'
-
-    $domain = 'https://github.com'
     @{
-        URL64   = $domain + $url64
+        URL64   = $url64
         Version = $version
     }
 }
