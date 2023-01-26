79c10e0b0d760b8c3a655e102502f3ac8469d778
takmg <takmg@norepry.github.com>
2023-01-26 21:27:23 +0900

    upgrade awssamcli

--- apps/awssamcli/tools/chocolateyinstall.ps1
+++ apps/awssamcli/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/aws/aws-sam-cli/releases/download/v1.70.0/AWS_SAM_CLI_64_PY3.msi'
-$checksum64 = '5cc065d4e8a789e3099204402cf775aa87b5110283dff1cb550aa27f13969fd2'
+$url64 = 'https://github.com/aws/aws-sam-cli/releases/download/v1.71.0/AWS_SAM_CLI_64_PY3.msi'
+$checksum64 = '37238f098e477817bf57e50dafcf0004ae74c4cfe42e31a3777ed14308cd9ed6'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
